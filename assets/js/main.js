e53d6e733c5432f7c26a8ea18114b1a142115c38
iprankurpandey <pprankur@gmail.com>
2021-10-29 14:49:35 +0530

    update : task done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-
+/**
+* Template Name: FlexStart - v1.7.0
+* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
   "use strict";
 
